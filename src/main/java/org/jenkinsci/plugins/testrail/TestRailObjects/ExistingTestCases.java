8f4b2623e430b5fb08ed6f855e9656943531fd6e
tdurova <tdurova@primervoto.de>
2018-07-26 15:40:43 +0200

    Delete not used import

--- src/main/java/org/jenkinsci/plugins/testrail/TestRailObjects/ExistingTestCases.java
+++ src/main/java/org/jenkinsci/plugins/testrail/TestRailObjects/ExistingTestCases.java
@@ -24,7 +24,6 @@ import org.jenkinsci.plugins.testrail.TestRailClient;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 /**
